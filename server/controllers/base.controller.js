c370ac70978e29176a03209b10a2ce90f6e8c09a
summerjacket <dxmcxd@gmail.com>
2018-12-23 18:02:08 -0800

    update base controller

--- server/controllers/base.controller.js
+++ server/controllers/base.controller.js
@@ -1,29 +1,29 @@
-const index = (req, res) => {
-  res.render("404");
+const index = (req, res, next) => {
+  next();
 };
 
-const create = (req, res) => {
-  res.render("404");
+const create = (req, res, next) => {
+  next();
 };
 
-const store = (req, res) => {
-  res.render("404");
+const store = (req, res, next) => {
+  next();
 };
 
-const show = (req, res) => {
-  res.render("404");
+const show = (req, res, next) => {
+  next();
 };
 
-const edit = (req, res) => {
-  res.render("404");
+const edit = (req, res, next) => {
+  next();
 };
 
-const update = (req, res) => {
-  res.render("404");
+const update = (req, res, next) => {
+  next();
 };
 
-const destroy = (req, res) => {
-  res.render("404");
+const destroy = (req, res, next) => {
+  next();
 };
 
 export default { index, create, store, show, edit, update, destroy };
