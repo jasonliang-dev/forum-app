fbdf4505f6f49c5666b5c3fc78dd5eb6693306b9
summerjacket <dxmcxd@gmail.com>
2018-12-24 21:59:40 -0800

    final commit before I really start on this project

--- server/routes/utils.js
+++ server/routes/utils.js
@@ -1,10 +1,13 @@
 // eslint-disable-next-line import/prefer-default-export
 export const resource = (router, route, controller) => {
-  router.get(`${route}/`, controller.index);
-  // router.get(`${route}/create`, controller.create);
+  // create
   router.post(`${route}/`, controller.store);
+  // read
+  router.get(`${route}/`, controller.index);
   router.get(`${route}/:id`, controller.show);
-  // router.get(`${route}/:id/edit`, controller.edit);
+  // update
   router.put(`${route}/:id`, controller.update);
+  // delete
   router.delete(`${route}/:id`, controller.destroy);
+  // it's C.R.U.D.!
 };
