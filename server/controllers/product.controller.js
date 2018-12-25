1b2c83bfeb22be2dee3c58895cb1d7a03ad32231
summerjacket <dxmcxd@gmail.com>
2018-12-24 22:26:04 -0800

    use promises in controller

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,41 +1,35 @@
 import Product from '../models/product.model';
 
 export const index = (req, res, next) => {
-  Product.find({}, (err, products) => {
-    if (err) next(err);
-    else res.send(products);
-  });
+  Product.find({})
+    .then(products => res.send(products))
+    .catch(next);
 };
 
 export const store = (req, res, next) => {
-  const product = new Product({
+  new Product({
     name: req.body.name,
     price: req.body.price,
-  });
-
-  product.save(err => {
-    if (err) next(err);
-    else res.send('Product Created successfully');
-  });
+  })
+    .save()
+    .then(() => res.send('Product Created successfully'))
+    .catch(next);
 };
 
 export const show = (req, res, next) => {
-  Product.findById(req.params.id, (err, product) => {
-    if (err) next(err);
-    else res.send(product);
-  });
+  Product.findById(req.params.id)
+    .then(product => res.send(product))
+    .catch(next);
 };
 
 export const update = (req, res, next) => {
-  Product.findByIdAndUpdate(req.params.id, { $set: req.body }, err => {
-    if (err) next(err);
-    else res.send('Product updated.');
-  });
+  Product.findByIdAndUpdate(req.params.id, { $set: req.body })
+    .then(() => res.send('product updated.'))
+    .catch(next);
 };
 
 export const destroy = (req, res, next) => {
-  Product.findByIdAndRemove(req.params.id, err => {
-    if (err) next(err);
-    else res.send('Deleted successfully!');
-  });
+  Product.findByIdAndRemove(req.params.id)
+    .then(() => res.send('Deleted successfully'))
+    .catch(next);
 };
