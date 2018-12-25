adc46a8fd2aadf098591a51a24dce4c9cc554d35
summerjacket <dxmcxd@gmail.com>
2018-12-25 00:11:02 -0800

    trim strings for some models

--- server/models/thread.model.js
+++ server/models/thread.model.js
@@ -3,7 +3,7 @@ import { Schema, model, SchemaTypes } from 'mongoose';
 const { ObjectId } = SchemaTypes;
 
 const ThreadModel = new Schema({
-  title: { type: String, required: true, max: 50 },
+  title: { type: String, required: true, trim: true, max: 50 },
   body: { type: String, required: true },
   viewCount: { type: Number, required: true, default: 0 },
   created: { type: Date, required: true, default: Date.now },
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,7 +3,8 @@ import { Schema, model, SchemaTypes } from 'mongoose';
 const { ObjectId } = SchemaTypes;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true, max: 20 },
+  email: { type: String, unique: true, required: true, trim: true },
+  username: { type: String, unique: true, required: true, trim: true, max: 20 },
   password: { type: String, required: true },
   about: { type: String },
   threads: [{ type: ObjectId, ref: 'thread' }],
