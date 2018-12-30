88b59225e0b1c32985d888c1823d53d5a10fae30
summerjacket <dxmcxd@gmail.com>
2018-12-29 23:33:08 -0800

    new thread component makes post request

--- client/src/views/NewThread/NewThread.jsx
+++ client/src/views/NewThread/NewThread.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import compose from 'ramda/src/compose';
 import Paper from '@material-ui/core/Paper';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import NewThreadForm from './NewThreadForm';
+import environment from '../../environment';
+import { inspect } from '../../utils';
 
 const styles = theme => ({
   paper: {
@@ -12,15 +17,26 @@ const styles = theme => ({
   },
 });
 
-const NewThread = ({ classes }) => (
+const handleSubmit = history => values => {
+  axios
+    .post(`${environment.endpoint}/threads`, values)
+    .then(() => history.push('/'))
+    .catch(inspect);
+};
+
+const NewThread = ({ classes, history }) => (
   <Paper className={classes.paper}>
     <Typography variant="h6">New Thread</Typography>
-    <NewThreadForm />
+    <NewThreadForm onSubmit={handleSubmit(history)} />
   </Paper>
 );
 
 NewThread.propTypes = {
   classes: PropTypes.shape({}).isRequired,
+  history: PropTypes.shape({}).isRequired,
 };
 
-export default withStyles(styles)(NewThread);
+export default compose(
+  withRouter,
+  withStyles(styles),
+)(NewThread);
