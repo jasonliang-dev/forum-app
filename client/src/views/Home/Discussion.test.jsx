7268f997f957d77f9e6232b0117ad167d7ff1f0f
summerjacket <dxmcxd@gmail.com>
2018-12-31 21:55:18 -0800

    add props to discussion test

--- client/src/views/Home/Discussion.test.jsx
+++ client/src/views/Home/Discussion.test.jsx
@@ -7,6 +7,9 @@ describe('Discussion', () => {
     const props = {
       title: 'foo',
       user: 'alice',
+      to: '/',
+      views: 0,
+      replyCount: 0,
     };
     shallow(<Discussion {...props} />);
   });
