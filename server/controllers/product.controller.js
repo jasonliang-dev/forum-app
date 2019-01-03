84d2bfe1d3bc38cfac665b2df7be31664482c0e0
summerjacket <dxmcxd@gmail.com>
2019-01-02 20:43:42 -0800

    remove product model and controller

--- server/controllers/product.controller.js
+++
@@ -1,6 +0,0 @@
-import Product from '../models/product.model';
-import BaseController from './base.controller';
-
-const ProductController = new BaseController(Product);
-
-export default ProductController;
